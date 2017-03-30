040be6d0d4ffc719e241df59e49cb9718e6eadf0
Andrew Jung <w0293459@nscc.ca>
2017-03-30 12:58:03 -0300

    Added models

---
+++ BeerDowser9000/BeerDowser9000/Models/BeerModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerDowser9000.Models
+{
+    class BeerModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string reviewlink { get; set; }
+        public string proxylink { get; set; }
+        public string blogmap { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string country { get; set; }
+        public string phone { get; set; }
+        public string url { get; set; }
+        public string overall { get; set; }
+        public string imagecount { get; set; }
+        List<ImageModel> images { get; set; }
+    }
+}
---
+++ BeerDowser9000/BeerDowser9000/Models/ImageModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerDowser9000.Models
+{
+    class ImageModel
+    {
+        public string imageid { get; set; }
+        public string directurl { get; set; }
+        public string imageurl { get; set; }
+        public string width { get; set; }
+        public string height { get; set; }
+        public string thumburl { get; set; }
+        public string caption { get; set; }
+        public string credit { get; set; }
+        public string crediturl { get; set; }
+        public string imagedate { get; set; }
+        public string score { get; set; }
+    }
+}
